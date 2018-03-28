a9eeaf48f4a85e51abac76557233e1861c7be102
Clark Alexander <calexander@neit.edu>
2018-03-28 10:35:20 -0400

    Finished table

--- mult.php
+++ mult.php
@@ -4,8 +4,20 @@ $table = "<table>\n";
 for ($rows = 1; $rows <= $size; $rows++)
 {
 	$table .= "\t<tr>";
+	for ($cols =1; $cols<=$size; $cols++):
+		$table .= "<td>" . $rows*$cols . "</td>";
+	endfor;
 	$table .= "\t</tr>\n";
 }
 $table .= "</table>\n";
-echo $table;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Multiplication Table</title>
+</head>
+<body>
+<?php echo $table; ?>
+</body>
+</html>
